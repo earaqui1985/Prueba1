817d041d00cc0625794d5aba0cc0df12d5e20c4f
Ethel Araya Quiros <ethelaraya@gmail.com>
2013-06-30 14:01:38 -0600

    CAMBIO 2

--- MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
+++ MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
@@ -15,6 +15,9 @@ namespace MvcApplication1.Controllers
             return new string[] { "value1", "value2" };
         }
 
+
+
+
         // GET api/values/5
         public string Get(int id)
         {
